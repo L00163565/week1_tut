70049c2c7b0e1e6b183a38829ca172c4f810090f
unknown <Snehal Shirsath>
2021-09-29 01:28:15 +0530

    q4 file added

---
+++ package1/q4.py
@@ -0,0 +1,19 @@
+# mathematics example
+
+ans_1 = 6 + 8 / 2 - 1  # this is the arithmetic operation of all integer values
+print(ans_1)
+ans_2 = 1 + 1 - 1 + 2 / 2  # this is the subtraction and addition of all the 1s
+print(ans_2)
+ans_3 = 1.1 + 5 / 2  # this operation contains floating value with integers, so result will be float value
+print(ans_3)
+ans_4 = 9.2 / 3  # this contains division of float value and integer type value
+print(ans_4)
+ans_5 = 9.2 % 3  # this example is of mod operation of two different data types
+print(ans_5)
+ans_6 = 1 * 2 + 3 * 5 % 4  # this is little complex multiplication,mod,addition operation of integers
+print(ans_6)
+ans_7 = 1 + 8 % 3 * 2 - 9  # this is example of subtracting higher number from lower so result will be negative value
+print(ans_7)
+ans_8 = 6 + (24 % (16 - 11))  # this contains brackets, so operation in the brackets will done first
+# and then the remaining will take place
+print(ans_8)
