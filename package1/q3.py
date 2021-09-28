c61ed4969090ae6b3e25152051a2e7139f656726
unknown <Snehal Shirsath>
2021-09-29 00:38:11 +0530

    q3 file added

---
+++ package1/q3.py
@@ -0,0 +1,14 @@
+
+
+# . ppsn # it is alphanumeric, can be difficult to store in particular variable
+# . passport Number # it is alphanumeric number
+# . temperature in degrees # it can be store in integer type
+# . cao number # it is also a alphanumeric number, may fail to decide correct type of variable to store it
+# . Miles travelled per day # it can be store in integer or floating type
+# . Average collection of exam grades # it can be integer or float value
+# . File path # it may contains string as well as symbols
+# . Person's middle initial # it can be store in to string variable
+# . Biography statement # it can be string or string plus numbers and symbols
+# . Book abstract # it can be string plus numbers also can contain special characters
+# . Mathematical function # it contains integer value as well as symbols
+
