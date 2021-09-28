f4216e9a3832b74ea5a75f0dc6aaaf2a73266287
unknown <Snehal Shirsath>
2021-09-28 19:50:17 +0530

    file q1 and q2 are added

---
+++ package1/q1.py
@@ -0,0 +1,23 @@
+print("\t " * 2 + "Title")
+print("Hello")
+print("Hello World")
+a = 5
+b = 12.3456789
+c = a + b
+
+d = "A number" + b  # string can be concat with string only
+# solution: type cast variable to string OR replace the string by other integer variable
+# ex: d = "A number" + "B number" OR d= a + b
+
+e = a + \
+    b + \
+    c
+f = 2
+g = a / f
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
---
+++ package1/q2.py
@@ -0,0 +1,21 @@
+
+
+top_icon = "^"
+bottom_icon = "v"
+left_icon = "<"
+right_icon = ">"
+
+print("Title")
+
+print(42*top_icon)
+print(left_icon, 10 * "\t", right_icon)
+print(left_icon, 10 * "\t", right_icon)
+print(left_icon, 4 * "\t", "Title", 5 * "\t", right_icon)
+print(left_icon, 10 * "\t", right_icon)
+print(left_icon, " A Option a", 7 * "\t", right_icon)
+print(left_icon, " B Option b", 7 * "\t", right_icon)
+print(left_icon, " C Option c", 7 * "\t", right_icon)
+print(left_icon, 10 * "\t", right_icon)
+print(left_icon, 10 * "\t", right_icon)
+print(42*bottom_icon)
+
